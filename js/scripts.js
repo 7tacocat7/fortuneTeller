4ded74c2f2e39c27d6aa0c6d42186d09f1e6415b
Carson Canales, Haneen Abukhater and Kate Byars <katebyars5@gmail.com>
2017-07-11 14:16:03 -0700

    fix list append issue in js for unlucky and lucky output

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,36 @@
 $(document).ready(function(){
 
   var unluckyItems = [];
-
+  var luckyItems =[];
   $("form#fortune").submit(function(event){
     event.preventDefault();
     $("#unluckyEvent").show();
+
     $("input:checkbox[name=unluckyEvent]:checked").each(function(){
       var unluckyFortune= $(this).val();
       unluckyItems.push(unluckyFortune);
-      $('#unluckyEvent').prepend(unluckyFortune + "<br>");
+      $('#unluckyEvent').append("<br>" + unluckyFortune + "<br>");
 
     });
 
     if (unluckyItems.length <= 3) {
-      alert("You are only a little bit unlucky!!!!")
+      alert("You are only a little bit unlucky!!!!");
+    }
+    else if(unluckyItems.length > 3){
+        alert("You are totally fucked");
     }
     // for ( var index = 0; index < unluckyItems.length; index += 1) {
     //
     // }
     $("#luckyEvent").show();
+
     $("input:checkbox[name=luckyEvent]:checked").each(function(){
       var luckyFortune = $(this).val();
-      $('#unluckyEvent').append(luckyFortune + "<br>");
+      luckyItems.push(luckyFortune);
+      $('#luckyEvent').append("<br>" +luckyFortune + "<br>");
     });
+    if (luckyItems.length <= 3) {
+      alert("You are only a little bit unlucky!!!!");
+    }
   });
 });
